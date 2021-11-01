29fcc47ff06634843403f2fee8b1ae1f3a8e203a
Deddy Wolley <deddywolley@gmail.com>
2021-11-01 08:59:07 +0800

    add home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+	config: {
+		initialColorMode: 'dark',
+		useSystemColorMode: false,
+	},
+	components: {
+		Button: {
+			baseStyle: { _focus: { boxShadow: 'none' } },
+		},
+		Input: {
+			baseStyle: {
+				_focus: {
+					boxShadow: 'none',
+					border: '1px solid',
+					borderColor: 'slate.500',
+				},
+			},
+		},
+	},
+	fonts: {
+		heading: 'Poppins',
+		body: 'Poppins',
+	},
+	styles: {
+		global: (props) => ({
+			'html, body': {
+				color: props.colorMode === 'dark' ? 'white' : 'gray.600',
+			},
+			a: {
+				color: props.colorMode === 'dark' ? 'teal.300' : 'teal.500',
+			},
+		}),
+	},
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
