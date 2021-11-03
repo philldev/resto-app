0fd2584282ed33551e06f514f81bf8914dff2f70
Deddy Wolley <deddywolley@gmail.com>
2021-11-03 14:39:18 +0800

    add auth context

---
+++ context/Auth.js
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import * as AuthApi from '../firebase/auth'
+import * as UserApi from '../firebase/user'
+
+const AuthContext = React.createContext(null)
+
+export const AuthProvider = ({ children }) => {
+	const [user, setUser] = React.useState(null)
+	const signup = async ({ email, password }) => {
+		try {
+			const newUserCred = await AuthApi.signup({ email, password })
+			await UserApi.createUser({ email, password, uid: newUserCred.user.uid })
+		} catch (error) {
+			throw error
+		}
+	}
+	const signin = async () => {
+		try {
+			await AuthApi.signout({ email, password })
+		} catch (error) {
+			throw error
+		}
+	}
+	const signout = async () => {
+		try {
+			await AuthApi.signout()
+		} catch (error) {
+			throw error
+		}
+	}
+	React.useEffect(() => {
+		const unsub = AuthApi.onAuthStateChanged(async (user) => {
+			if (user) {
+				try {
+					const userData = await UserApi.getUser(user.uid)
+					setUser(userData)
+				} catch (error) {
+					console.log('error auth')
+				}
+			} else {
+				setUser(null)
+			}
+		})
+		return () => {
+			unsub()
+		}
+	}, [])
+	const value = { user, signup, signin, signout }
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => {
+	const context = React.useContext(AuthContext)
+	if (context === undefined) throw new Error('Auth Context not provided')
+	return context
+}
