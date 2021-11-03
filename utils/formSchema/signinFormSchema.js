5e690dacebb4fb1d118d11959d20b866276fd92d
Deddy Wolley <deddywolley@gmail.com>
2021-11-04 06:35:33 +0800

    add form schema

---
+++ utils/formSchema/signinFormSchema.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const SigninSchema = yup
+	.object({
+		email: yup.string().required('Email is required').email('Email is invalid'),
+		password: yup
+			.string()
+			.required('Password is required')
+			.min(6, 'Password must be longer than 6 characters'),
+	})
+	.required()
+
+const signinResolver = yupResolver(SigninSchema)
+
+export { signinResolver, SigninSchema }
---
+++ utils/formSchema/signupFormSchema.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const SignupSchema = yup
+	.object({
+		email: yup.string().required('Email is required').email('Email is invalid'),
+		password: yup
+			.string()
+			.required('Password is required')
+			.min(6, 'Password must be longer than 6 characters'),
+	})
+	.required()
+
+const signupResolver = yupResolver(SignupSchema)
+
+export { SignupSchema, signupResolver }
