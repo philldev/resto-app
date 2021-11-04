1cf19ae021ab8532e608b96a2437545e20efba2e
Deddy Wolley <deddywolley@gmail.com>
2021-11-04 09:35:56 +0800

    add app page

--- pages/app.js
+++ pages/app.js
@@ -1,5 +1,23 @@
+import { Button } from '@chakra-ui/button'
+import { Box, Heading } from '@chakra-ui/layout'
+import * as React from 'react'
+import LoaderPage from '../components/common/LoaderPage'
 import Page from '../components/common/Page'
+import { useAuth } from '../context/auth'
+import useProtectedRoute from '../hooks/useProtectedRoute'
 
 export default function App() {
-	return <Page>App</Page>
+	const user = useProtectedRoute()
+	const { signout } = useAuth()
+	if (user === undefined) return <LoaderPage />
+	if (!user) return null
+	if (user)
+		return (
+			<Page>
+				<Heading>App</Heading>
+				<Box>
+					<Button onClick={signout}>Logout</Button>
+				</Box>
+			</Page>
+		)
 }
