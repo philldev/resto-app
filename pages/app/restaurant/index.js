2b94aa657ad537eebc74f0272b4c959598d7bb13
Deddy Wolley <deddywolley@gmail.com>
2021-11-14 01:05:36 +0800

    update resto settings page

--- pages/app/restaurant/index.js
+++ pages/app/restaurant/index.js
@@ -1,6 +1,21 @@
 import { Button } from '@chakra-ui/button'
+import { useDisclosure } from '@chakra-ui/hooks'
 import { ChevronLeftIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons'
 import { Box, Flex, Text } from '@chakra-ui/layout'
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogOverlay,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+} from '@chakra-ui/modal'
 import { AppPage } from '../../../components/common/AppPage'
 import { HomeIcon } from '../../../components/common/icons/HomeIcon'
 import withProtectedRoute from '../../../components/hoc/withProtectedRoute'
@@ -27,29 +42,13 @@ function Restaurant() {
 							borderBottom='5px solid'
 							borderBottomColor='teal.500'
 						>
-							<Text fontSize='4xl'>{currentResto.name}'s Resto</Text>
+							<Text fontSize='4xl'>{currentResto.name}&apos;s Resto</Text>
 							<Text fontSize='xl'>{currentResto.address}</Text>
 						</Flex>
 					</Flex>
 					<Flex flexDir='column' p='4' pt='0'>
-						<Button
-							variant='ghost'
-							rounded='none'
-							textAlign='left'
-							justifyContent='flex-start'
-							leftIcon={<EditIcon />}
-						>
-							Edit Resto
-						</Button>
-						<Button
-							variant='ghost'
-							rounded='none'
-							textAlign='left'
-							justifyContent='flex-start'
-							leftIcon={<DeleteIcon />}
-						>
-							Hapus Resto
-						</Button>
+						<EditResto resto={currentResto} />
+						<DeleteResto resto={currentResto} />
 						<Button
 							variant='ghost'
 							rounded='none'
@@ -63,6 +62,88 @@ function Restaurant() {
 				</Flex>
 			</Flex>
 		</AppPage>
+	)
+}
+
+const DeleteResto = ({ resto }) => {
+	const [isOpen, setIsOpen] = React.useState(false)
+	const onClose = () => setIsOpen(false)
+	const onOpen = () => setIsOpen(true)
+	const cancelRef = React.useRef()
+	return (
+		<>
+			<Button
+				onClick={onOpen}
+				variant='ghost'
+				rounded='none'
+				textAlign='left'
+				justifyContent='flex-start'
+				leftIcon={<DeleteIcon />}
+			>
+				Hapus Resto
+			</Button>
+			<AlertDialog
+				isCentered
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent mx='4' bg='gray.800'>
+						<AlertDialogHeader fontSize='lg'>
+							Hapus Resto{' '}
+							<Text as='span' fontWeight='bold'>
+								{resto.name}
+							</Text>
+						</AlertDialogHeader>
+						<AlertDialogBody>
+							Are you sure? You can&apos;t undo this action afterwards.
+						</AlertDialogBody>
+						<AlertDialogFooter>
+							<Button ref={cancelRef} onClick={onClose}>
+								Tidak
+							</Button>
+							<Button colorScheme='red' onClick={onClose} ml={3}>
+								Ya
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</>
+	)
+}
+
+const EditResto = ({ resto }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	return (
+		<>
+			<Button
+				onClick={onOpen}
+				variant='ghost'
+				rounded='none'
+				textAlign='left'
+				justifyContent='flex-start'
+				leftIcon={<EditIcon />}
+			>
+				Edit Resto
+			</Button>
+			<Modal isCentered isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent bg='gray.800' mx='4'>
+					<ModalHeader>Tambah Resto</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<RestoForm
+							isEditing
+							resto={resto}
+							onSuccess={onClose}
+							onCancel={onClose}
+						/>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
 	)
 }
 
